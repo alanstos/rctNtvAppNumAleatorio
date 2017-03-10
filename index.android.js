707c7973841f8c0cc5516ac88323b37625ff77f8
alan <alanstos@gmail.com>
2017-03-10 11:48:34 -0300

    add botao de gerar numero - simples

--- index.android.js
+++ index.android.js
@@ -1,15 +1,31 @@
 var React = require('react');
+var View = require('react-native').View;
 var Text = require('react-native').Text;
+var Button = require('react-native').Button;
 var AppRegistry = require('react-native').AppRegistry;
 
-const App = function(){
+const geraNumeroAleatorio = () => {
+  var numero_aleatorio = Math.random();
+  numero_aleatorio = numero_aleatorio * 10;
+  numero_aleatorio = Math.floor(numero_aleatorio);
+
+  alert(numero_aleatorio);
+};
+
+const App = () => {
 
   return (
-      <Text>
-        Meu ola mundo!!!
-      </Text>
+      <View>
+        <Text>
+          Meu ola mundo!!! 4
+        </Text>
+        <Button 
+          title="Gerar número aleatório"
+          onPress={geraNumeroAleatorio}
+        />
+      </View>
     );
 
 };
 
-AppRegistry.registerComponent('rctNtvAppNumAleatorio', function(){ return App});
+AppRegistry.registerComponent('rctNtvAppNumAleatorio', () => App);
